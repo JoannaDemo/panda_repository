b168cfecf0de620e5f4db86b97a6a7c6a2fd62b2
JoannaDemo <69588787+JoannaDemo@users.noreply.github.com>
2020-09-14 00:06:50 +0200

    Update PandaApplicationSeleniumTest.java

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -1,61 +1,46 @@
 package pl.pandait.panda;
 
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.Assert.assertEquals;
 
-
-@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
-
     private static WebDriver driver;
 
-    @LocalServerPort
-    private int port;
-
-    @BeforeEach
-    public void startup() throws InterruptedException {
-
+    @BeforeAll
+    public static void startup() throws InterruptedException {
         //Driver znajduje się w resource
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver");
         //Ścieżka do Firefoxa - jeżeli nie działa trzeba sprawdzić, gdzie FF jest zainstalowany!
-        System.setProperty("webdriver.firefox.bin", "/usr/lib/firefox/firefox");
+        System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
+        FirefoxOptions capabilities = new FirefoxOptions();
+        capabilities.setCapability("marionette", true);
 
         // Tworzymy nową instancję Firefoxa
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--headless");
         driver = new FirefoxDriver(capabilities);
-
+        // Otwieramy stronę
         // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
-        driver.get(String.format("http://192.168.44.44:%d/", port));
-
+        driver.get("http://localhost:8080/");
+        // Wyświetlamy informacje, że udało się otwozyć stronę
+        System.out.println("Successfully opened the website");
         //Czekamy 2 sekundy
         Thread.sleep(2000);
     }
 
     @Test
-    public void greetings_shouldOpenMainPageThenReturnWelcomeText() {
+    public void checkIfApplicationRunCorrectly(){
         System.out.println("Uruchamiam test 1: Sprawdzenie napisu na stronie głównej");
-        WebElement greetingElement = driver.findElement(By.xpath("//p"));
-        String greetingText = greetingElement.getText().trim();
-        assertEquals("Get your greeting here", greetingText);
-    }
+        WebElement greetingString = driver.findElement(By.xpath("//p"));
+        String napis = greetingString.getText().trim();
+        assertEquals("Get your greeting here", napis);
 
-    @Test
-    public void greetings_shouldOpenSubpageThenReturnGreetingsText() {
-        System.out.println("Uruchamiam test 2: Sprawdzenie napisu na podstronie");
-        WebElement greetingElement = driver.findElement(By.xpath("//p"));
-        WebElement linkToGreetings = greetingElement.findElement(By.xpath("./a"));
+        WebElement linkToGreetings = greetingString.findElement(By.xpath("./a"));
         linkToGreetings.click();
 
         WebElement helloWorldString = driver.findElement(By.xpath("//p"));
@@ -63,9 +48,8 @@ public class PandaApplicationSeleniumTest {
         assertEquals("Hello, World!", newPageString);
     }
 
-
-    @AfterEach
-    public void after() {
-        driver.close();
+    @AfterAll
+    public static void after(){
+        driver.quit();
     }
 }
