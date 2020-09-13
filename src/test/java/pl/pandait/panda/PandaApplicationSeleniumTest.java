c56294a292f38570ad35a49c3e7bab7fc29e5647
JoannaDemo <69588787+JoannaDemo@users.noreply.github.com>
2020-09-14 00:19:12 +0200

    Update PandaApplicationSeleniumTest.java

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -3,7 +3,6 @@ import java.net.URL;
 import java.net.MalformedURLException;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
